9d1ae16252542336611724698d0f88774c4e3853
ghRuby0 <rjsh42K@gmail.com>
2026-09-20 16:06:29 +1000

    ran more + cleaned

--- cleanlists.py
+++ cleanlists.py
@@ -10,29 +10,15 @@ cwd = Path(os.getcwd())
 # Overwrites the checkedwords.txt file to prevent repeats
 with open(str(cwd) + "".join(["\\", CHECKEDFILE, "\\checkedwords.txt"]), 'r') as file:
     checked_words = list(set([line.strip() for line in file]))
-checked_words = set(checked_words)
+checked_words = sorted(list(set(checked_words)))
 with open(str(cwd) + "".join(["\\", CHECKEDFILE, "\\checkedwords.txt"]), 'w') as file:
     file.write('\n'.join(checked_words))
 
 # Over-writes the checkedleaderboard.txt file to prevent repeats
 with open(str(cwd) + "".join(["\\", CHECKEDFILE, "\\checkedleaderboard.txt"]), 'r') as file:
     checked_words = list(set([line.strip() for line in file]))
-checked_words = set(checked_words)
+checked_words = sorted(list(set(checked_words)))
 with open(str(cwd) + "".join(["\\", CHECKEDFILE, "\\checkedleaderboard.txt"]), 'w') as file:
-    file.write('\n'.join(checked_words))
-
-# Re-writes the checkedwords.txt file in alphabetical order
-with open(str(cwd) + "".join(["\\", CHECKEDFILE, "\\checkedwords.txt"]), 'r') as file:
-    checked_words = list(set([line.strip() for line in file]))
-checked_words = sorted(checked_words)
-with open(str(cwd) + "".join(["\\", CHECKEDFILE, "\\checkedwords_cleaned.txt"]), 'w') as file:
-    file.write('\n'.join(checked_words))
-
-# Re-writes the checkedleaderboard.txt file in alphabetical order
-with open(str(cwd) + "".join(["\\", CHECKEDFILE, "\\checkedleaderboard.txt"]), 'r') as file:
-    checked_words = list(set([line.strip() for line in file]))
-checked_words = sorted(checked_words)
-with open(str(cwd) + "".join(["\\", CHECKEDFILE, "\\checkedleaderboard_cleaned.txt"]), 'w') as file:
     file.write('\n'.join(checked_words))
 
 # Re-writes the checkedleaderboard.txt file in score order
